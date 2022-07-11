baa4971e976c88f9faa082d5398ebeeeaa94709b
Akash Verma <vermaakash60584@gmail.com>
2022-07-11 11:09:26 +0530

    Third Commit

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace EmployeeDetails
             services.AddControllersWithViews();
 
             services.AddDbContext<EmployeeDetailsContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("EmployeeDetailsContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("EmployeeConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
