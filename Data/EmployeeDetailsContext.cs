39b2fc85cc1be13664bb556bc0a8b2ae13adf081
Akash Verma <vermaakash60584@gmail.com>
2022-07-11 11:06:12 +0530

    Second Commit

---
+++ Data/EmployeeDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeDetails.Models;
+
+namespace EmployeeDetails.Data
+{
+    public class EmployeeDetailsContext : DbContext
+    {
+        public EmployeeDetailsContext (DbContextOptions<EmployeeDetailsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeDetails.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeDetails.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesignation { get; set; }
+        public DateTime EDOJ { get; set; }
+
+    }
+}
